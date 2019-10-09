df40bb090632609f3faf67782265d508a3f01fb7
tawandamuyambo <tawandamuyambo@tawanda.muyambo>
2019-10-09 09:32:10 +0200

    mywebapp

--- src/main/java/com/furniture/controllers/ViewProducts.java
+++ src/main/java/com/furniture/controllers/ViewProducts.java
@@ -5,6 +5,7 @@
  */
 package com.furniture.controllers;
 
+import com.furniture.controllers.exceptions.NonexistentEntityException;
 import com.furniture.entities.Products;
 import static com.sun.corba.se.spi.presentation.rmi.StubAdapter.request;
 import java.io.IOException;
@@ -24,13 +25,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import com.furniture.entities.Users;
 import org.springframework.web.servlet.ModelAndView;
 import zw.co.paynow.core.Payment;
 import zw.co.paynow.core.Paynow;
 import zw.co.paynow.responses.StatusResponse;
 import zw.co.paynow.responses.WebInitResponse;
+import com.furniture.entities.PaymentInfo;
+import java.math.BigDecimal;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceUnit;
+import org.springframework.web.bind.annotation.ResponseBody;
 import com.furniture.entities.PaymentInfo;
 /**
  *
@@ -52,6 +57,72 @@ public class ViewProducts {
     return model;
     
     }
+ 
+  @RequestMapping(value="/editProducts", method = RequestMethod.GET)  
+ public ModelAndView editProducts(ModelAndView model){
+        ProductsJpaController pj = new ProductsJpaController();
+       
+        List<Products> p = pj.selectAll();
+        model.setViewName("itemList.jsp");
+        model.addObject( "products",p);
+        model.addObject( "sizo",p.size());
+        
+    return model;
+    
+    }
+ 
+
+  @RequestMapping(value="/viewItem", method = RequestMethod.POST)  
+ public ModelAndView viewItem(ModelAndView model, HttpServletRequest request,HttpServletResponse response){
+
+    System.out.println("kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk");
+    ProductsJpaController pj = new ProductsJpaController();
+    System.out.println("id id"+request.getParameter("inputId"));
+    int prodId = Integer.parseInt(request.getParameter("inputId"));
+    Products prod = pj.findProducts(prodId);
+        //pj.destroy(prodId);
+       
+        model.setViewName("editItem.jsp");
+        model.addObject( "item",prod);
+        
+    return model;
+    
+    } 
+ 
+  @RequestMapping(value="/editItem", method = RequestMethod.POST)  
+ public ModelAndView editItem(ModelAndView model, HttpServletRequest request,HttpServletResponse response){
+
+    System.out.println("editing id");
+    ProductsJpaController pj = new ProductsJpaController();
+    System.out.println("edit id"+request.getParameter("inputId"));
+    int prodId = Integer.parseInt(request.getParameter("inputId"));
+    Products prod = pj.findProducts(prodId);
+      //  pj.destroy(prodId);
+       
+     //   List<Products> p = pj.selectAll();
+        model.setViewName("editItem.jsp");
+        model.addObject( "products",prod);
+//        model.addObject( "sizo",p.size());
+        
+    return model;
+    
+    } 
+ 
+  @RequestMapping(value="/removeItem", method = RequestMethod.POST)  
+ public ModelAndView removeItem(ModelAndView model,HttpServletRequest request,HttpServletResponse response) throws NonexistentEntityException{
+   System.out.println("kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk");
+    ProductsJpaController pj = new ProductsJpaController();
+      System.out.println("id id"+request.getParameter("inputId"));
+        int prodId = Integer.parseInt(request.getParameter("inputId"));
+        pj.destroy(prodId);
+        List<Products> p = pj.selectAll();
+        model.setViewName("itemList.jsp");
+        model.addObject( "products",p);
+        model.addObject( "sizo",p.size());
+        
+    return model;
+    }
+ 
     
    ModelAndView AddProducts(List<Products>products, HttpServletRequest request,ModelAndView model){
        
@@ -77,10 +148,41 @@ public class ViewProducts {
         int total=0;
         
         for(Products item: products){
-        total+= item.getPrice()*item.getQuantity();
+        total+= item.getZwlprice()*item.getQuantity();
         }
         return total;
     }
+    
+    @RequestMapping(value="/loginUser", method = RequestMethod.POST)          
+    public ModelAndView loginUser(HttpServletRequest request,HttpServletResponse response, ModelAndView model){
+    String username = request.getParameter("username");
+    String password = request.getParameter("password");
+    ProductsJpaController pj = new ProductsJpaController();       
+    List<Users> p = pj.selectAllUsers();
+    
+    if(!p.isEmpty()){
+        //login success
+        model.setViewName("updateRate.jsp");
+        
+    }
+    else
+        model.setViewName("failed_login.jsp");
+        //failed jsp
+    return model;
+        
+    }
+    
+    
+  ////////
+        @RequestMapping(value="/update", method = RequestMethod.POST)          
+    public ModelAndView update(HttpServletRequest request,HttpServletResponse response, ModelAndView model){        
+      ProductsJpaController pj = new ProductsJpaController();       
+      pj.updateRate(Float.parseFloat(request.getParameter("rate")));
+      model.setViewName("success.jsp");
+      return model;
+    }
+  ////////
+    
     
  @RequestMapping(value="/checkOut", method = RequestMethod.POST)          
     public ModelAndView checkOut(HttpServletRequest request,HttpServletResponse response, ModelAndView model){
@@ -119,6 +221,8 @@ public class ViewProducts {
    prod.setProductId(a.getProductId());
    prod.setDescription(a.getDescription());
    prod.setPrice(a.getPrice());
+   prod.setZwlprice(a.getZwlprice());
+   prod.setRate(a.getRate());
    prod.setProductName(a.getProductName());
    prod.setQuantity(integerQuantities[x]);
    cart.add(prod);
@@ -159,7 +263,8 @@ if (cart== null){
    ProductsJpaController pj=new ProductsJpaController();
    Products a=pj.findProducts(productId);
    prod.setDescription(a.getDescription());
-   prod.setPrice(a.getPrice());
+   prod.setZwlprice(a.getZwlprice());
+   //prod.setPrice(a.getPrice());
    prod.setProductName(a.getProductName());
    prod.setQuantity(quantity);
    
@@ -173,7 +278,9 @@ if (cart== null){
    ProductsJpaController pj=new ProductsJpaController();
    Products a=pj.findProducts(productId);
    prod.setDescription(a.getDescription());
-   prod.setPrice(a.getPrice());
+   prod.setZwlprice(a.getZwlprice());
+
+//   prod.setPrice(a.getPrice());
    prod.setProductName(a.getProductName());
    prod.setQuantity(quantity);
    
@@ -244,12 +351,13 @@ public ModelAndView continueShopping(ModelAndView model, HttpServletRequest requ
 
 public ModelAndView paymentInfo(ModelAndView model, HttpServletRequest request,HttpServletResponse response) throws MessagingException, ServletException, IOException{
    //should we carry the session here as well
+   String name, surname, companyName, email, phonenumber, comment, address;
    HttpSession session1 = request.getSession(false);
    session1.getAttribute("cart");
    PaymentInfoJpaController pic = new PaymentInfoJpaController();
    PaymentInfo p=pic.addInfo(request);
-   PayNowPaymnet pp = new PayNowPaymnet();
-   pp.makePaymnet(request, response,p.getEmail());
+   ViewProducts pp = new ViewProducts();
+   pp.makePaymnet(request, response,p.getEmail(), p.getId());
    
    model.setViewName("checkout.jsp");
    return model;
@@ -376,6 +484,22 @@ public ModelAndView updateCart(ModelAndView model,HttpServletRequest request,Htt
 }
 
 
+/* public ModelAndView returnPage(ModelAndView model, HttpServletRequest req){
+     System.out.println("now in return page ");
+     String[] itemIds = req.getParameterValues("itemId");
+     
+
+     
+        model.setViewName("return_page.jsp");
+        
+    return model;
+    
+    }*/
+
+
+
+
+
  @RequestMapping(value="/add", method = RequestMethod.POST)  
  public ModelAndView add(ModelAndView model, HttpServletRequest req){
      System.out.println("noe on add");
@@ -425,20 +549,29 @@ public ModelAndView processUpdate(ModelAndView model,HttpServletRequest request,
 }        
   
 
-class PayNowPaymnet{
+        String INTEGRATION_ID="8382";
+    String INTEGRATION_KEY="ae0d2249-4693-4da1-a2e6-102685b31038";
+   
+    Paynow paynow = new Paynow(INTEGRATION_ID, INTEGRATION_KEY);
+    String pollUrl= new String();
+    String aa = new String();
+    HttpSession session = null;
+//save message in session
 
-public void makePaymnet (HttpServletRequest request, HttpServletResponse response1, String email) throws ServletException, IOException{
-
+public ModelAndView makePaymnet (HttpServletRequest request, HttpServletResponse response1, String email,int id) throws ServletException, IOException{
+ModelAndView model = new ModelAndView();
 HttpSession session1 = request.getSession(false);
    session1.getAttribute("cart");
    List<Products> cart = (List<Products>) session1.getAttribute("cart");
    
-   String INTEGRATION_ID="3270";
-   String INTEGRATION_KEY="7b57815a-b2e7-47ae-a57a-cb3044d5850d";
-   
-    Paynow paynow = new Paynow(INTEGRATION_ID, INTEGRATION_KEY);
+ /*  String INTEGRATION_ID="8106";
+   String INTEGRATION_KEY="aa22a573-95ff-4c04-a277-4e3895161203";*/
+
+
 
         Payment payment = paynow.createPayment("Invoice 35",email);
+        paynow.setResultUrl("http://localhost:8080/onlineShop/returnPage?id="+id); //post on merchant website
+        paynow.setReturnUrl("http://localhost:8080/onlineShop/returnPage?id="+id); //result page for customer
 
         // Passing in the name of the item and the price of the item
         for(Products item: cart){
@@ -447,47 +580,114 @@ HttpSession session1 = request.getSession(false);
         payment.add(item.getProductName(), itemTotalPrice);
       //  payment
         }
-
-        //Initiating the transaction
-        WebInitResponse response = paynow.send(payment);
+        WebInitResponse response = null;
         
+        //Initiating the transaction
+        try{
+        response = paynow.send(payment);
+        
+        System.out.println("response is: "+response.isRequestSuccess());
         //If a mobile transaction,
         //MobileInitResponse response = paynow.sendMobile(payment, "0771234567", MobileMoneyMethod.ECOCASH);
 
+//save message in session
+        
+        
+        
+        
         if (response.isRequestSuccess()) {
             // Get the url to redirect the user to so they can make payment
             String redirectURL = response.redirectURL();
+System.out.println("redirectURL is: "+response.redirectURL());
 
             // Get the poll url of the transaction
-            String pollUrl = response.pollUrl();
+            pollUrl = response.pollUrl();  
 
-            
+System.out.println("pollUrl is: "+response.pollUrl());
+
+// find by id and update pollurll
+  PaymentInfoJpaController abc = new PaymentInfoJpaController();
+PaymentInfo prod =  abc.findPaymentInfo(id);
+prod.setPollurl(pollUrl);
+abc.edit(prod);
             
             //////////
            String targetURL = redirectURL;
+System.out.println("targetURL is: "+redirectURL);
+
 
 response1.sendRedirect(targetURL);
             
             
             //////////
   
-           
-            //checking if the payment has been paid
-            StatusResponse status = paynow.pollTransaction(pollUrl);
 
-            if (status.paid()) {
-                // Yay! Transaction was paid for
-            } else {
-                System.out.println("Why you no pay?");
-            }
 
         } else {
             // Something went wrong
             System.out.println(response.errors());
         }
+        
+        }
+        catch(Exception ex){
+            System.out.println("Something went wrong: "+ex);
+            model.setViewName("error.jsp");              
+
+        
+        }
+        
+        return model;
 
 }
 
+
+ @RequestMapping(value="/returnPage", method = RequestMethod.GET)  
+public ModelAndView checkTransaction(ModelAndView model, HttpServletRequest req){
+/*
+    private final String merchantReference;
+    private final String paynowReference;
+    private final BigDecimal amount;
+    private final boolean paid;*/
+            int id= Integer.parseInt(req.getParameter("id"));
+            
+            //find transaction by id
+            PaymentInfoJpaController cc = new PaymentInfoJpaController();
+            PaymentInfo aa= cc.findPaymentInfo(id);
+            String poller= aa.getPollurl();
+            //checking if the payment has been paid
+            StatusResponse status = paynow.pollTransaction(poller);
+
+            if (status.paid()) {
+                System.out.println("Payment Succeeded");
+                model.addObject("response",status);
+                model.setViewName("return_page.jsp");              
+                
+                // Yay! Transaction was paid for
+            } else {
+                int amount=0;
+                String paynowref="";
+                System.out.println("Payment Failed. Why you no pay?");
+                amount=status.getAmount().intValue();
+                paynowref=status.getPaynowReference();
+                model.addObject("amount",amount);
+                model.addObject("paynowref",paynowref);
+                model.addObject("state", status);
+               /* model.addObject("reference",status.getMerchantReference());
+                model.addObject("paynowreference",status.getPaynowReference());
+                model.addObject("status",status.getStatus());
+                model.addObject("pollurl",status.getAmount());
+                model.addObject("hash",status.getRawResponseContent());*/
+
+
+                model.setViewName("return_page.jsp");              
+                //model.setViewName("payment_failed.jsp");
+            }
+            
+            return model;
+
+
 }
+
+
 
 }
